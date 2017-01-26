56f5ecb87279e716efdb863082d4ad9294ffab92
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-01-26 19:35:55 +0300

    Create mysql_tunning.sh

---
+++ mysql_tunning.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Скрипт настройки сервера MySQL 5.x/6.x
+# Автор:  Олег Букатчук
+# Версия: 0.1
+# e-mail: oleg@bukatchuk.com
+
+# Объявляем переменные для авторизации в MySQL
+export HOST_MYSQL=some_localhost
+export USER_MYSQL=some_login
+export PASS_MYSQL=some_password
+export DB_MYSQL=some_database
