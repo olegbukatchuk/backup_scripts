be0be22d0929775acd6fb49ab02d065f8458bedb
Oleg Bukatchuk <oleg@bukatchuk.com>
2017-01-28 20:44:09 +0300

    Мелкие исправления

--- mysql_tunning.sh
+++ mysql_tunning.sh
@@ -7,11 +7,6 @@
 
 # Подключаем файл c настройками DB Suite
 . ./db_suite.conf
-<<<<<<< HEAD
-=======
 
 # Подключаем FTP и монтируем его в директорию $FTP
 curlftpfs -v -o iocharset=UTF-8 ftp://user:password@ftp.domain.ru/ /mnt/ftp
-
-
->>>>>>> dev
